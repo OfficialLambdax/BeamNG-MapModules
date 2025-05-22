885e79e0efc9d27b9957613323760f3cb5062456
Lambdax <91626975+OfficialLambdax@users.noreply.github.com>
2025-05-22 14:34:09 +0200

    Rework module loader to be cosistent with game own extensions loader, also pcall load/unload

--- levels/your_map_name/mainLevel.lua
+++ levels/your_map_name/mainLevel.lua
@@ -1,75 +1,102 @@
 -- Made by Neverless @ BeamMP. Problems, Questions or requests? Feel free to ask.
 
--- hotreloads the mainLevel lua
--- extensions.reload("levels_" .. core_levels.getLevelName(getMissionFilename()) .. "_mainLevel")
+--[[
+	hotreloads the mainLevel lua
+		extensions.reload("levels_" .. core_levels.getLevelName(getMissionFilename()) .. "_mainLevel")
+	
+	alternate
+		extensions.unload("mainLevel"); extensions.loadAtRoot("levels/" .. core_levels.getLevelName(getMissionFilename()) .. "/mainLevel", "")
+]]
 
-local M = {}
-
--- add module names that are found in the /levels/yourlevel/lua/* path
-local MY_MODULES = {"triggerlights", "portals"}
-
-local INITIALIZED = false
+local M = {
+	_VERSION = '0.2' -- 22.05.2025 (DD.MM.YYYY)
+}
 local MODULES = {}
+
+
+-- -------------------------------------------------------------------
+-- Common
+local function myLevelName()
+	local source = debug.getinfo(1).source
+	if source == nil then return end
+	
+	local strip = source:sub(8)
+	local _, pos = strip:find('/')
+	return strip:sub(1, pos - 1)
+end
+
+local function fileName(path)
+	local str = path:sub(1):gsub("\\", "/")
+	local _, pos = str:find(".*/")
+	if pos == nil then return path end
+	return str:sub(pos + 1, -1)
+end
+
+local function fileExtension(path)
+	return path:match("[^.]+$")
+end
+
+local function fileNameNoExt(path)
+	local file_name = fileName(path)
+	local ext = (fileExtension(file_name) or ''):lower()
+	if ext ~= "lua" then return end
+	
+	return file_name:sub(1, #file_name - #ext - 1)
+end
 
 -- -------------------------------------------------------------------
 -- Module loader
-local function loadLevelModule(level_name, module)
-	local path = 'levels/' .. level_name .. '/lua/' .. module
-	if not FS:fileExists(path .. '.lua') then
-		log('E', 'Module could not be found "' .. module .. '"')
+local function loadLevelModule(path)
+	local module = fileNameNoExt(path)
+	if not module then return end -- not a lua file
+	
+	local ok, r = pcall(extensions.loadAtRoot, path:sub(1, #path - 4), "")
+	if not ok then
+		log('E', 'mainLevel.module_loader', r)
 		return
 	end
-	if MODULES[module] then return end
-	
-	extensions.loadAtRoot(path, "")
-	log('I', 'Loaded level module "' .. module .. '"')
+	log('I', 'mainLevel.module_loader', 'Loaded level module "' .. module .. '"')
 	
 	local ext = _G[module]
-	if ext then
-		MODULES[module] = module
-		if ext.onWorldReadyState then ext.onWorldReadyState(2) end
-	end
+	if ext then MODULES[module] = module end
 end
 
 -- -------------------------------------------------------------------
 -- Load/Unload
 local function init()
-	local level_name = core_levels.getLevelName(getMissionFilename())
-	if level_name == nil then return end
-	INITIALIZED = true -- doing it here in case of loading error with any of the extensions
-	
-	for _, module in ipairs(MY_MODULES) do
-		loadLevelModule(level_name, module)
+	local level_name = myLevelName()
+	log('I', 'mainLevel.module_loader', 'Loading modules for map "' .. level_name .. '"')
+	for _, path in ipairs(FS:directoryList('levels/' .. level_name .. '/lua/') or {}) do
+		loadLevelModule(path)
 	end
 end
 
 local function unload()
+	log('I', 'mainLevel.module_loader', 'Unloading all map modules')
 	for module, _ in pairs(MODULES) do
-		extensions.unload(module)
-		log('I', 'Unloaded level module "' .. module .. '"')
+		local ok, r = pcall(extensions.unload, module)
+		if not ok then
+			log('E', 'mainLevel.module_loader', r)
+		else
+			log('I', 'mainLevel.module_loader', 'Unloaded level module "' .. module .. '"')
+		end
 	end
 	MODULES = {}
-	INITIALIZED = false
+end
+
+M.reload = function()
+	unload()
+	init()
 end
 
 -- -------------------------------------------------------------------
 -- Game events
-M.onUpdate = function() -- there is no event that is fired when mainLevel is loaded. onClientPreStartMission should be, but isnt
-	if not INITIALIZED then init() end
-end
-
 M.onExtensionLoaded = function() -- ran by game on load and for manual reloads
 	init()
 end
 
 M.onExtensionUnloaded = function()
 	unload()
-end
-
-M.onWorldReadyState = function(state)
-	if state == 2 then
-		init()
-	end
 end
 
 return M
